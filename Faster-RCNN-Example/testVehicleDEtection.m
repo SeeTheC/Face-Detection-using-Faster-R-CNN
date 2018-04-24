4496b2ef34f2495b50aa57251fe8a7294cadb9cd
SeeTheC <khursheed.ali1991@gmail.com>
2018-04-24 16:47:43 +0530

    fixed bug. Checkpoint:1

--- Faster-RCNN-Example/testVehicleDEtection.m
+++ Faster-RCNN-Example/testVehicleDEtection.m
@@ -31,50 +31,50 @@ idx = floor(0.6 * height(vehicleDataset));
 trainingData = vehicleDataset(1:idx,:);
 testData = vehicleDataset(idx:end,:);
 
-%% Creating CNN layers
-% Create image input layer.
-inputLayer = imageInputLayer([32 32 3]);
-
-% Define the convolutional layer parameters.
-filterSize = [3 3];
-numFilters = 32;
-
-% Create the middle layers.
-middleLayers = [
-                
-    convolution2dLayer(filterSize, numFilters, 'Padding', 1)   
-    reluLayer()
-    convolution2dLayer(filterSize, numFilters, 'Padding', 1)  
-    reluLayer() 
-    maxPooling2dLayer(3, 'Stride',2)    
-    
-    ];
-
-finalLayers = [
-    
-        % Add a fully connected layer with 64 output neurons. The output size
-        % of this layer will be an array with a length of 64.
-        fullyConnectedLayer(64)
-
-        % Add a ReLU non-linearity.
-        reluLayer()
-
-        % Add the last fully connected layer. At this point, the network must
-        % produce outputs that can be used to measure whether the input image
-        % belongs to one of the object classes or background. This measurement
-        % is made using the subsequent loss layers.
-        fullyConnectedLayer(width(vehicleDataset))
-
-        % Add the softmax loss layer and classification layer. 
-        softmaxLayer()
-        classificationLayer()
-    ];
-
-layers = [
-    inputLayer
-    middleLayers
-    finalLayers
-    ]
+% %% Creating CNN layers
+% % Create image input layer.
+% inputLayer = imageInputLayer([32 32 3]);
+% 
+% % Define the convolutional layer parameters.
+% filterSize = [3 3];
+% numFilters = 32;
+% 
+% % Create the middle layers.
+% middleLayers = [
+%                 
+%     convolution2dLayer(filterSize, numFilters, 'Padding', 1)   
+%     reluLayer()
+%     convolution2dLayer(filterSize, numFilters, 'Padding', 1)  
+%     reluLayer() 
+%     maxPooling2dLayer(3, 'Stride',2)    
+%     
+%     ];
+% 
+% finalLayers = [
+%     
+%         % Add a fully connected layer with 64 output neurons. The output size
+%         % of this layer will be an array with a length of 64.
+%         fullyConnectedLayer(64)
+% 
+%         % Add a ReLU non-linearity.
+%         reluLayer()
+% 
+%         % Add the last fully connected layer. At this point, the network must
+%         % produce outputs that can be used to measure whether the input image
+%         % belongs to one of the object classes or background. This measurement
+%         % is made using the subsequent loss layers.
+%         fullyConnectedLayer(width(vehicleDataset))
+% 
+%         % Add the softmax loss layer and classification layer. 
+%         softmaxLayer()
+%         classificationLayer()
+%     ];
+% 
+% layers = [
+%     inputLayer
+%     middleLayers
+%     finalLayers
+%     ]
 %% Creating Options
 
 % Options for step 1.
@@ -123,7 +123,9 @@ if doTrainingAndEval
     
     % Train Faster R-CNN detector. Select a BoxPyramidScale of 1.2 to allow
     % for finer resolution for multiscale object detection.
-    detector = trainFasterRCNNObjectDetector(trainingData, layers, options, ...
+    net = alexnet;
+%     detector = trainFasterRCNNObjectDetector(trainingData,net,options);
+    detector = trainFasterRCNNObjectDetector(trainingData, net, options, ...
         'NegativeOverlapRange', [0 0.3], ...
         'PositiveOverlapRange', [0.6 1], ...
         'BoxPyramidScale', 1.2);
